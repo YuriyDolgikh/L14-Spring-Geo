d3c83097238c5ae72c476432fb156f2a41bf7e78
Dolgikh Yuriy <dolgih.yv@gmail.com>
2023-12-16 09:48:09 +0100

    First working version

---
+++ src/main/java/ua/kiev/prog/retrievers/RateRetriever.java
@@ -0,0 +1,20 @@
+package ua.kiev.prog.retrievers;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import ua.kiev.prog.json.Rate;
+
+@Component
+public class RateRetriever {
+
+    private static final String URL = "http://data.fixer.io/api/latest?access_key=KuLSGIPuZZ1Ka2XL07dNWGpCwOcV2jpo";
+
+    @Cacheable("rates") // Redis
+    public Rate getRate() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Rate> response = restTemplate.getForEntity(URL, Rate.class);
+        return response.getBody();
+    }
+}
